ebd7cd62d6a6089eda9715add36f7b827cf870aa
MURAKAMI Masahiko <fossamagna2@gmail.com>
2022-07-04 22:43:30 +0900

    add authorId column and FK

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,13 +4,18 @@ import { Post } from "./entity/Post";
 import { Author } from "./entity/Author";
 import { CreatePostTable1656926770819 } from "./migration/1656926770819-CreatePostTable";
 import { CreateAuthorTable1656939116999 } from "./migration/1656939116999-CreateAuthorTable";
+import { AddAuthorIdColumn1656939646470 } from "./migration/1656939646470-AddAuthorIdColumn";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "local.sqlite",
   synchronize: true,
-  logging: false,
+  logging: true,
   entities: [Post, Author],
-  migrations: [CreatePostTable1656926770819, CreateAuthorTable1656939116999],
+  migrations: [
+    CreatePostTable1656926770819,
+    CreateAuthorTable1656939116999,
+    AddAuthorIdColumn1656939646470,
+  ],
   subscribers: [],
 });
--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+import { Author } from "./Author"
 
 @Entity()
 export class Post {
@@ -10,4 +11,7 @@ export class Post {
 
   @Column()
   text: string
+
+  @ManyToOne((type) => Author, { cascade: true, nullable: false })
+  author: Author;
 }
