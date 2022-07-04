c04b2562e272a88c104554fb1819642ef37c42ac
MURAKAMI Masahiko <fossamagna2@gmail.com>
2022-07-04 21:59:34 +0900

    add author entity

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,16 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Post } from "./entity/Post";
+import { Author } from "./entity/Author";
 import { CreatePostTable1656926770819 } from "./migration/1656926770819-CreatePostTable";
+import { CreateAuthorTable1656939116999 } from "./migration/1656939116999-CreateAuthorTable";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "local.sqlite",
   synchronize: true,
   logging: false,
-  entities: [Post],
-  migrations: [CreatePostTable1656926770819],
+  entities: [Post, Author],
+  migrations: [CreatePostTable1656926770819, CreateAuthorTable1656939116999],
   subscribers: [],
 });
